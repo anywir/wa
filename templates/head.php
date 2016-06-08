b36ea01f03715f96249f47fb954875a4629d936d
anywir <akio@bk.ru>
2016-06-08 19:52:46 +0300

    modalogin

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
     <meta name="title" content="wired audio" >
     <meta name="description" content="DIY audio, аудіо для ентузіастів, ЦАП высокой верности, сетевой проигрыватель" >
     <meta name="keywords" content="DIY DAC ЦАП AUDIO аудио Hi-Fi">
-    <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="<?=SITE?>lib/images/favicon.png" type="image/png">
     <link rel="stylesheet" type="text/css" href="<?=SITE?>lib/css/wir.css">
     <link rel="stylesheet" type="text/css" href="<?=SITE?>lib/css/adopt.css">
     <link rel="stylesheet" type="text/css" href="<?=SITE?>lib/css/modal.css">
